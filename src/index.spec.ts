03e71063140937cf7a6c1ae9457609d5b499a380
Oleksii Shnyra <shnura94@gmail.com>
2020-05-02 13:25:58 +0300

    test(base validator): transform method

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -13,4 +13,15 @@ describe('valid', () => {
         expect(user).toBeInstanceOf(UserDto)
         expect(user).toEqual({ name: 'username' })
     })
+
+    it('transformation method', () => {
+        const incrementer = (n: number) => n + 1
+        const validator = v.Number().transform(incrementer)
+
+        const initialValue = 10
+        const expected = incrementer(initialValue)
+        const result = validator.validateAndTransform(initialValue)
+
+        expect(result).toBe(expected)
+    })
 })
